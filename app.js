7c0ed50eedbb765d9265bd63bd31681a9de6509f
김한빈 <hanbin@gimhanbin-ui-MacBook-Air.local>
2017-07-18 12:27:21 +0900

    sync-async example, modified app.js

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const express = require('express');
 var app = express();
 
-app.listen(3000, ()=>{console.log('Connect 3000 port!');});
+app.get('/', (req, res)=>{
+  res.send('Hello home page');
+});
+app.get('/login', (req, res)=>{
+  res.send('<h1>Login please<h1>');
+});
+app.listen(3000, ()=>{
+  console.log('Connected 3000 port!');
+});
