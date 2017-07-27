61f9a0a5fa0c60f0d6c25cb384550ccf79240221
toctocbin <haanbin@naver.com>
2017-07-27 15:55:36 +0900

    add cookie security

--- app_cookie.js
+++ app_cookie.js
@@ -1,7 +1,7 @@
 const express = require('express');
 var cookieParser = require('cookie-parser');
 var app = express();
-app.use(cookieParser());
+app.use(cookieParser('2D3Z%dfasZ!q123#'));
 
 var products = {
   1:{title:'The history of web 1'},
@@ -11,25 +11,67 @@ app.get('/products', (req, res)=>{
   var output = '';
   for(var name in products){
     output += `
-
     <li>
       <a href="/cart/${name}"> ${products[name].title}
     </li>`;
     }
     res.send(`<h1>products</h1><ul>${output}</ul><a href="/cart">Cart</a>`);
 });
+/*
+cart = {
+  id : 수량
+  1:count
+  2:count
+}
 
+*/
+app.get('/cart/:id', (req, res)=>{
+  var id = req.params.id;
+  var cart = {};
+  if (req.signedCookies.cart) {
+    cart = req.signedCookies.cart;
+  }else {
+      cart = {};
+  }
+  if (!cart[id]) {
+    cart[id] = 0;
+  }
+  cart[id] = parseInt(cart[id])+1;
+  res.cookie('cart', cart, {signed:true});
+  res.redirect('/cart');
+
+});
+
+app.get('/cart', (req, res)=>{
+  var cart = req.signedCookies.cart;
+  if (!cart) {
+    res.send('Empty!');
+  } else {
+    var output = '';
+    for(var id in cart){
+      output += `
+      <li>
+        ${products[id].title} (${cart[id]})
+      </li>`;
+    }
+    res.send(`
+      <h1>Cart</h1>
+      <ul>${output}</ul>
+      <a href="/products">Products List</a>
+      `);
+  }
+});
 
 
 
 app.get('/count', (req, res)=>{
    var count;
-  if (req.cookies.count) {
-    count = parseInt(req.cookies.count);
+  if (req.signedCookies.count) {
+    count = parseInt(req.signedCookies.count);
   }else {
     count = 0;
   }
-  res.cookie('count', count+1);
+  res.cookie('count', count+1, {signed:true});
   res.send('count : '+ count);
 
 });
